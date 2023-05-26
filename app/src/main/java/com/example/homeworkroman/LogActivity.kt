f39fccab7c1ee2e2c51b11de6f9bf622e69a512b
Roman <125905743+RomanZuyonok@users.noreply.github.com>
2023-05-26 06:51:15 -0700

    Revert "Merge pull request #2"
    This reverts commit cdbf6d6d39e849e06fea8c69bfde42d5e8583629.

--- app/src/main/java/com/example/homeworkroman/LogActivity.kt
+++ app/src/main/java/com/example/homeworkroman/LogActivity.kt
@@ -1,17 +1,19 @@
 package com.example.homeworkroman
 
 import android.content.Intent
-import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
-
+    }
+    fun onSignUp(view: View){
         val onClickTextView = findViewById<TextView>(R.id.log_text_view2)
-        onClickTextView.setOnClickListener {
+        onClickTextView.setOnClickListener{
             val intent = Intent(this@LogActivity, SignActivity::class.java)
             startActivity(intent)
         }
