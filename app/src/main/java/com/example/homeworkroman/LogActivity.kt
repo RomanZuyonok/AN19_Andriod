cdbf6d6d39e849e06fea8c69bfde42d5e8583629
Roman <125905743+RomanZuyonok@users.noreply.github.com>
2023-05-26 06:37:39 -0700

    Merge pull request #2
    * Added homework 2 and added homework 1 p.s. (partly fix)
    
    * Merge remote-tracking branch 'origin/develop' into develop

--- app/src/main/java/com/example/homeworkroman/LogActivity.kt
+++ app/src/main/java/com/example/homeworkroman/LogActivity.kt
@@ -1,19 +1,17 @@
 package com.example.homeworkroman
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
-    }
-    fun onSignUp(view: View){
+
         val onClickTextView = findViewById<TextView>(R.id.log_text_view2)
-        onClickTextView.setOnClickListener{
+        onClickTextView.setOnClickListener {
             val intent = Intent(this@LogActivity, SignActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/homeworkroman/SignActivity.kt
+++ app/src/main/java/com/example/homeworkroman/SignActivity.kt
@@ -1,20 +1,16 @@
 package com.example.homeworkroman
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
-    }
-
-    fun onLogIn(view: View){
         val onClickTextView = findViewById<TextView>(R.id.sign_text_view2)
-        onClickTextView.setOnClickListener{
+        onClickTextView.setOnClickListener {
             val intent = Intent(this@SignActivity, LogActivity::class.java)
             startActivity(intent)
         }
